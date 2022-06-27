0eb0aafcead66bb0b83a887e447868613dcef22c
BelikJulia <43135868+BelikJulia@users.noreply.github.com>
2022-06-27 17:04:49 +0300

    Add files via upload

---
+++ Lab1/Ex2-3/Starter/WpfApplication/WpfApplication/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApplication
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void testButton_Click(object sender, RoutedEventArgs e)
+        {
+            string line = testInput.Text;
+            line = line.Replace(",", " y:");
+            line = "x:" + line;
+            formattedText.Text += line + "\n";
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            formattedText.Text = "";
+        }
+
+        private void Window_Loaded_1(object sender, RoutedEventArgs e)
+        {
+            string line = "";
+            formattedText.Text = line;
+            while ((line = Console.ReadLine()) != null)
+            {
+                line = line.Replace(",", " y:");
+                line = "x:" + line;
+                formattedText.Text += line + "\n";
+            }
+        }
+
+    }
+}
