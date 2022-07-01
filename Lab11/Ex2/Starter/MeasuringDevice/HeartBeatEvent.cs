9219d90c4c6bd1a78e155add59bca3cd67eac6bd
BelikJulia <43135868+BelikJulia@users.noreply.github.com>
2022-07-01 10:06:29 +0300

    lab 11 2

---
+++ Lab11/Ex2/Starter/MeasuringDevice/HeartBeatEvent.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace MeasuringDevice
+{
+    public class HeartBeatEventArgs : EventArgs
+    {
+        public DateTime TimeStamp { get; private set; }
+        public HeartBeatEventArgs() : base()
+        {
+            this.TimeStamp = DateTime.Now;
+        }
+    }
+    public delegate void HeartBeatEventHandler(object sender, HeartBeatEventArgs args);
+}
