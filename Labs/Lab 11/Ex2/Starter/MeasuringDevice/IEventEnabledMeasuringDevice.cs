8eab4fa8eba03f9e520e4a0fce8e23d45d3d01b5
BelikJulia <43135868+BelikJulia@users.noreply.github.com>
2022-10-28 12:47:01 +0300

    Summer labs

---
+++ Labs/Lab 11/Ex2/Starter/MeasuringDevice/IEventEnabledMeasuringDevice.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MeasuringDevice
+{
+    interface IEventEnabledMeasuringDevice : IMeasuringDevice
+    {
+        event EventHandler NewMeasurementTaken;
+
+        // Define the new event in the interface.
+        // Event that fires every heartbeat.
+        event HeartBeatEventHandler HeartBeat;
+        // Define the HeartBeatInterval property in the interface.
+        // Read only heartbeat interval - set in constructor.
+        int HeartBeatInterval { get; }
+    }
+}
