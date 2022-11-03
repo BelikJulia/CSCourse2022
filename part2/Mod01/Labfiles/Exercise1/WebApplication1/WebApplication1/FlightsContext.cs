92e1be7492b971f34ac49a0bf35dd415f8433016
BelikJulia <43135868+BelikJulia@users.noreply.github.com>
2022-11-03 13:35:09 +0300

    2mod 1

---
+++ part2/Mod01/Labfiles/Exercise1/WebApplication1/WebApplication1/FlightsContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebApplication1
+{
+    public class FlightsContext : DbContext
+    {
+        public FlightsContext(DbContextOptions<FlightsContext> options) : base(options){}
+
+        public DbSet<Flight> Flight { get; set; }
+    }
+}
