a0b0dac20d35d42279dd274ab78d08b8c17d49ed
BelikJulia <43135868+BelikJulia@users.noreply.github.com>
2022-11-30 10:20:50 +0300

    2mod 2.1 2.1 3.1 3.2 4.1

---
+++ part2/Mod04/LabFiles/Lab1/Starter/BlueYonder.Hotels/BlueYonder.Hotels.DAL/Repository/IHotelBookingRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using BlueYonder.Hotels.DAL.Models;
+
+namespace BlueYonder.Hotels.DAL.Repository
+{
+    interface IHotelBookingRepository
+    {
+        IEnumerable<Room> GetAvaliabileByDate(DateTime date);
+        IEnumerable<Reservation> GetAllReservation();
+        Task DeleteReservation(int reservationId);
+    }
+}
